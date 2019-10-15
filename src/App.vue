c65e39b5d89cb474a608a23f42fb5f0aabdaf3b7
Joshua Flanagan <josh_flanagan@live.co.uk>
2019-10-15 20:48:34 +0100

    Created todo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,90 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="todoMain">
+    <h1>{{title}}</h1>
+    <input type="text" class="nes-input" placeholder="Add todo..." v-on:keyup.enter="addTodo">
+    <ul>
+      <li v-bind:key="todo" v-for="todo in todos" class="flex">
+        <label>
+          <input type="checkbox" class="nes-checkbox" v-model="todo.done">
+          <span>&nbsp;</span>
+        </label>
+        <del v-if="todo.done">{{todo.text}}</del>
+        <span v-else>{{todo.text}}</span>
+        <div class="space"></div>
+        <button class="nes-btn is-error padding" v-on:click="removeTodo(todo.id)">X</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
+  data: () => {
+    return {
+      title: 'TODOS',
+      todos: [
+      ],
+    }
+  },
   components: {
-    HelloWorld
+  },
+  methods: {
+    addTodo(event) {
+      const text = event.target.value;
+      this.todos.push({text, done: false, id: Date.now()});
+      event.target.value = '';
+    },
+    removeTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    },
+    check(todo) {
+      todo.done = !todo.done
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body,
+pre,
+code,
+kbd,
+samp {
+  font-family: "Press Start 2P";
 }
+
+body {
+  background: #20262E;
+  padding: 20px;
+}
+
+#todoMain {
+  background: #fff;
+  border-radius: 4px;
+  padding: 20px;
+  transition: all 0.2s;
+}
+
+li {
+  margin: 8px 0;
+}
+
+del {
+  color: rgba(0, 0, 0, 0.3);
+}
+
+.padding {
+  padding: 1px 7px 2px;
+}
+
+.flex {
+  display: flex;
+}
+
+.space {
+  flex-grow: 1;
+}
+
 </style>
